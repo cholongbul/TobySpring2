13d051ac8664b9f413cb39f1ce0b340d968b6330
meyoub612 <meyoub612@naver.com>
2021-06-12 23:50:55 +0900

    Map형식으로 sql 추출

--- toby2/src/springbook/user/AOP/dao/UserDaoJdbc.java
+++ toby2/src/springbook/user/AOP/dao/UserDaoJdbc.java
@@ -3,6 +3,7 @@ package springbook.user.AOP.dao;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 
 import javax.sql.DataSource;
 
@@ -16,43 +17,12 @@ import springbook.user.domain.User2;
 //스프링의 JdbcTemplate
 public class UserDaoJdbc implements UserDao {
 
-	private String sqlAdd;
-	private String sqlGet;
-	private String sqlGetAll;
-	private String sqlDeleteAll;
-	private String sqlGetCount;
-	private String sqlUpdate;
+	
 
-	public void setSqlAdd(String sqlAdd) {
-		this.sqlAdd = sqlAdd;
-	}
-
-	public void setSqlGet(String sqlGet) {
-		this.sqlGet = sqlGet;
-	}
-
-	public void setSqlGetAll(String sqlGetAll) {
-		this.sqlGetAll = sqlGetAll;
-	}
-
-	public void setSqlDeleteAll(String sqlDeleteAll) {
-		this.sqlDeleteAll = sqlDeleteAll;
-	}
-
-	public void setSqlGetCount(String sqlGetCount) {
-		this.sqlGetCount = sqlGetCount;
-	}
-
-	public void setSqlUpdate(String sqlUpdate) {
-		this.sqlUpdate = sqlUpdate;
-	}
-
-	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
-		this.jdbcTemplate = jdbcTemplate;
-	}
-
-	public void setUserMapper(RowMapper<User2> userMapper) {
-		this.userMapper = userMapper;
+	private Map<String, String> sqlMap;
+	
+	public void setSqlMap(Map<String, String> sqlMap) {
+		this.sqlMap = sqlMap;
 	}
 
 	private JdbcTemplate jdbcTemplate;// 기존 JdbcContext에서 JdbcTemplate로
@@ -78,34 +48,34 @@ public class UserDaoJdbc implements UserDao {
 
 	public void add(final User2 user) {// 클라이언트 final변수
 
-		this.jdbcTemplate.update(this.sqlAdd, // 외부 주입 받은 sql사용
+		this.jdbcTemplate.update(this.sqlMap.get("add"), // 외부 주입 받은 sql사용
 				user.getId(), user.getName(), user.getPassword(), user.getLevel().intValue(), user.getLogin(),
 				user.getRecommend(), user.getEmail());
 	}
 
 	public User2 get(String id) {
-		return this.jdbcTemplate.queryForObject(this.sqlGet, new Object[] { id }, this.userMapper);
+		return this.jdbcTemplate.queryForObject(this.sqlMap.get("get"), new Object[] { id }, this.userMapper);
 
 	}
 
 	public List<User2> getAll() {
-		return this.jdbcTemplate.query(this.sqlGetAll, this.userMapper);
+		return this.jdbcTemplate.query(this.sqlMap.get("getAll"), this.userMapper);
 	}
 
 	// 데이터 모두 삭제
 	public void deleteAll() {
-		this.jdbcTemplate.update(this.sqlDeleteAll);// 내장 콜백 사용
+		this.jdbcTemplate.update(this.sqlMap.get("deleteAll"));// 내장 콜백 사용
 	}
 
 	// 테이블 레코드 갯수 리턴
 	public int getCount() {
 
-		return this.jdbcTemplate.queryForInt(this.sqlGetCount);// 위의 코드 압축버전
+		return this.jdbcTemplate.queryForInt(this.sqlMap.get("getCount"));// 위의 코드 압축버전
 	}
 
 	@Override
 	public void update(User2 user) {
-		this.jdbcTemplate.update(this.sqlUpdate, user.getName(), user.getPassword(), user.getLevel().intValue(),
+		this.jdbcTemplate.update(this.sqlMap.get("update"), user.getName(), user.getPassword(), user.getLevel().intValue(),
 				user.getLogin(), user.getRecommend(), user.getEmail(), user.getId());
 
 	}
